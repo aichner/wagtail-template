2dbf208897763bcc6f6b87efb9d54d8b907814a5
kleberbaum <kleberbaum@erebos.xyz>
2020-08-01 12:18:28 +0200

    Fix a typo in base settings
    A newline has been added before the licence tag.

--- esite/settings/base.py
+++ esite/settings/base.py
@@ -266,5 +266,6 @@ PATTERN_LIBRARY_TEMPLATE_DIR = "templates"
 # > System Checks
 # Wagtail forms not used so silence captcha warning
 SILENCED_SYSTEM_CHECKS = ["captcha.recaptcha_test_key_error"]
+
 # SPDX-License-Identifier: (EUPL-1.2)
 # Copyright © 2019 Werbeagentur Christian Aichner
