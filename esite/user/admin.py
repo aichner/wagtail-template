6c3e3066e60da2c49e4866f49b2ea324fb2b72f9
kleberbaum <kleberbaum@erebos.xyz>
2020-08-04 14:43:25 +0200

    Remove user admin
    The remains of an admin configuration for the user have been removed.

--- esite/user/admin.py
+++ esite/user/admin.py
@@ -9,29 +9,5 @@ from .models import User
 # Register your user related models here.
 
 
-class UserAdmin(ModelAdmin):
-    model = User
-    menu_label = "User"
-    menu_icon = "user"
-    menu_order = 290
-    add_to_settings_menu = False
-    exclude_from_explorer = False
-
-    # Listed in the user overview
-    list_display = ("date_joined", "username", "email")
-    search_fields = ("date_joined", "username", "email")
-
-
-class CustomerAdminB(ModelAdminGroup):
-    menu_label = "User Management"
-    menu_icon = "group"
-    menu_order = 110
-    add_to_settings_menu = False
-    exclude_from_explorer = False
-    items = (UserAdmin,)
-
-
-# modeladmin_register(CustomerAdminB)
-
 # SPDX-License-Identifier: (EUPL-1.2)
 # Copyright © 2019-2020 Werbeagentur Christian Aichner
